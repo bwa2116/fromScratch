93254277d238f5de61f03126acde18cabed37b08
bwa2116 <151790222+bwa2116@users.noreply.github.com>
2024-03-15 21:43:12 +0100

    Update data.py

--- data.py
+++ data.py
@@ -43,3 +43,43 @@ def prepare_CIFAR10_data(batch_size=4, num_workers=2, train_sample_size=None, te
     classes = ('plane', 'car', 'bird', 'cat',
             'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
     return trainloader, testloader, classes
+
+
+def prepare_MNIST_data(batch_size=4, num_workers=2, train_sample_size=None, test_sample_size=None):
+    train_transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Resize((28, 28)),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.RandomResizedCrop((28, 28), scale=(0.8, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=2),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    trainset = torchvision.datasets.MNIST(root='../data', train=True,
+                                            download=True, transform=train_transform)
+    if train_sample_size is not None:
+        # Randomly sample a subset of the training set
+        indices = torch.randperm(len(trainset))[:train_sample_size]
+        trainset = torch.utils.data.Subset(trainset, indices)
+
+
+
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                            shuffle=True, num_workers=num_workers)
+
+    test_transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Resize((28, 28)),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    testset = torchvision.datasets.MNIST(root='../data', train=False,
+                                        download=True, transform=test_transform)
+    if test_sample_size is not None:
+        # Randomly sample a subset of the test set
+        indices = torch.randperm(len(testset))[:test_sample_size]
+        testset = torch.utils.data.Subset(testset, indices)
+
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                            shuffle=False, num_workers=num_workers)
+
+    classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+    
+    return trainloader, testloader, classes
