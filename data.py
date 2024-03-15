fe654df64b4c1b3546169cb9af97c5c86ce552f2
bwa2116 <151790222+bwa2116@users.noreply.github.com>
2024-03-15 20:34:33 +0100

    Update and rename CIFAR10.py to data.py

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ import torchvision
 import torchvision.transforms as transforms
 
 
-def prepare_data(batch_size=4, num_workers=2, train_sample_size=None, test_sample_size=None):
+def prepare_CIFAR10_data(batch_size=4, num_workers=2, train_sample_size=None, test_sample_size=None):
     train_transform = transforms.Compose(
         [transforms.ToTensor(),
         transforms.Resize((32, 32)),
@@ -13,7 +13,7 @@ def prepare_data(batch_size=4, num_workers=2, train_sample_size=None, test_sampl
         transforms.RandomResizedCrop((32, 32), scale=(0.8, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=2),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+    trainset = torchvision.datasets.CIFAR10(root='../data', train=True,
                                             download=True, transform=train_transform)
     if train_sample_size is not None:
         # Randomly sample a subset of the training set
@@ -30,7 +30,7 @@ def prepare_data(batch_size=4, num_workers=2, train_sample_size=None, test_sampl
         transforms.Resize((32, 32)),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+    testset = torchvision.datasets.CIFAR10(root='../data', train=False,
                                         download=True, transform=test_transform)
     if test_sample_size is not None:
         # Randomly sample a subset of the test set
