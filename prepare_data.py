d49a39859443b4a9a4f0a6b41df6a80a5a22f4d1
bwa2116 <151790222+bwa2116@users.noreply.github.com>
2024-03-16 13:07:04 +0100

    Update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -119,7 +119,6 @@ def prepare_Places365_data(batch_size=4, num_workers=2, train_sample_size=None, 
 
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                             shuffle=False, num_workers=num_workers)
-
-    classes = ('plane', 'car', 'bird', 'cat',
-            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    import places365classes
+    classes = places365classes
     return trainloader, testloader, classes
