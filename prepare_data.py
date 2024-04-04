9288e7fbf90b7aec271126706cf45af605ccf26d
bwa2116 <151790222+bwa2116@users.noreply.github.com>
2024-04-04 22:13:51 +0200

    Update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -133,39 +133,42 @@ def prepare_Places365_data(batch_size=4, num_workers=2, train_sample_size=100000
 
 
 
-def prepare_ImageNet_data(batch_size=4, num_workers=2, train_sample_size=100000, test_sample_size=25000):
+def prepare_ImageNet_data(batch_size=4, num_workers=2, train_sample_size=None, test_sample_size=None):
+
     train_transform = transforms.Compose(
         [transforms.ToTensor(),
-        transforms.Resize((64, 64)),
+        # transforms.Resize((256, 256)),
+        transforms.Resize((64, 64)), 
         transforms.RandomHorizontalFlip(p=0.5),
-        transforms.RandomResizedCrop((32, 32), scale=(0.8, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=2),
+        # transforms.RandomResizedCrop((256, 256), scale=(0.8, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=2),
+        transforms.RandomResizedCrop((64, 64), scale=(0.8, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=2),
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
 
-    # trainset = torchvision.datasets.CIFAR10(root='../data', train=True,
-    #                                         download=True, transform=train_transform)
+    trainset = torchvision.datasets.ImageFolder('tiny-imagenet-200/train', transform=train_transform)
+    
     if train_sample_size is not None:
         # Randomly sample a subset of the training set
         indices = torch.randperm(len(trainset))[:train_sample_size]
         trainset = torch.utils.data.Subset(trainset, indices)
 
-    # trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-    #                                         shuffle=True, num_workers=num_workers)
-
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                            shuffle=True, num_workers=num_workers)
+    
     test_transform = transforms.Compose(
         [transforms.ToTensor(),
+        # transforms.Resize((256, 256)),
         transforms.Resize((64, 64)),
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
 
-    # testset = torchvision.datasets.CIFAR10(root='../data', train=False,
-    #                                     download=True, transform=test_transform)
+    testset = torchvision.datasets.ImageFolder('tiny-imagenet-200/val', transform=test_transforms)
+    
     if test_sample_size is not None:
         # Randomly sample a subset of the test set
         indices = torch.randperm(len(testset))[:test_sample_size]
         testset = torch.utils.data.Subset(testset, indices)
 
-    # testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
-    #                                         shuffle=False, num_workers=num_workers)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                            shuffle=False, num_workers=num_workers)
 
-    from imagenetclasses import imagenet_classes
-    classes = imagenet_classes
+    classes = []
     return trainloader, testloader, classes
