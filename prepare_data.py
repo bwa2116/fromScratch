4897be6f0b2bfecbbd2da7551da0baffb7ccd39c
bwa2116 <151790222+bwa2116@users.noreply.github.com>
2024-04-04 22:59:53 +0200

    Update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -141,10 +141,10 @@ def prepare_ImageNet200_data(batch_size=4, num_workers=2, train_sample_size=None
     
     train_transform = transforms.Compose(
         [transforms.ToTensor(),
-        # transforms.Resize((256, 256)),
+        # transforms.Resize((224, 224)),
         transforms.Resize((64, 64)), 
         transforms.RandomHorizontalFlip(p=0.5),
-        # transforms.RandomResizedCrop((256, 256), scale=(0.8, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=2),
+        # transforms.RandomResizedCrop((224, 224), scale=(0.8, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=2),
         transforms.RandomResizedCrop((64, 64), scale=(0.8, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=2),
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
 
@@ -160,7 +160,7 @@ def prepare_ImageNet200_data(batch_size=4, num_workers=2, train_sample_size=None
     
     test_transform = transforms.Compose(
         [transforms.ToTensor(),
-        # transforms.Resize((256, 256)),
+        # transforms.Resize((224, 224)),
         transforms.Resize((64, 64)),
         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
 
