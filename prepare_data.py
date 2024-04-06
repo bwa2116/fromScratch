42695221208967aba14e5ef6406874d3207f14cd
bwa2116 <151790222+bwa2116@users.noreply.github.com>
2024-04-06 11:56:30 +0200

    Update prepare_data.py

--- prepare_data.py
+++ prepare_data.py
@@ -138,41 +138,54 @@ def prepare_ImageNet200_data(batch_size=4, num_workers=2, train_sample_size=None
     from tiny_img import download_tinyImg200
     if not os.path.exists('./tiny-imagenet-200/'):
         download_tinyImg200('.')
-    
-    train_transform = transforms.Compose(
-        [transforms.ToTensor(),
-        # transforms.Resize((224, 224)),
-        transforms.Resize((64, 64)), 
-        transforms.RandomHorizontalFlip(p=0.5),
-        # transforms.RandomResizedCrop((224, 224), scale=(0.8, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=2),
-        transforms.RandomResizedCrop((64, 64), scale=(0.8, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=2),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
-    trainset = torchvision.datasets.ImageFolder('tiny-imagenet-200/train', transform=train_transform)
-    
-    if train_sample_size is not None:
-        # Randomly sample a subset of the training set
-        indices = torch.randperm(len(trainset))[:train_sample_size]
-        trainset = torch.utils.data.Subset(trainset, indices)
+    dataset = torchvision.datasets.ImageFolder('tiny-imagenet-200/train', transform=transforms.ToTensor())
+    test_dataset = torchvision.datasets.ImageFolder('tiny-imagenet-200/val', transform=transforms.ToTensor())
+    train_dataset, val_dataset = torch.utils.data.random_split(dataset, [80000, 20000])
+    test_dataset, val_dataset = torch.utils.data.random_split(val_dataset, [10000, 10000])
 
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+    trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
                                             shuffle=True, num_workers=num_workers)
-    
-    test_transform = transforms.Compose(
-        [transforms.ToTensor(),
-        # transforms.Resize((224, 224)),
-        transforms.Resize((64, 64)),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
 
-    testset = torchvision.datasets.ImageFolder('tiny-imagenet-200/val', transform=test_transform)
-    
-    if test_sample_size is not None:
-        # Randomly sample a subset of the test set
-        indices = torch.randperm(len(testset))[:test_sample_size]
-        testset = torch.utils.data.Subset(testset, indices)
-
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+    testloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
                                             shuffle=False, num_workers=num_workers)
+    
+    # train_transform = transforms.Compose(
+    #     [transforms.ToTensor(),
+    #     # transforms.Resize((224, 224)),
+    #     transforms.Resize((64, 64)), 
+    #     transforms.RandomHorizontalFlip(p=0.5),
+    #     # transforms.RandomResizedCrop((224, 224), scale=(0.8, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=2),
+    #     transforms.RandomResizedCrop((64, 64), scale=(0.8, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=2),
+    #     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+
+    # trainset = torchvision.datasets.ImageFolder('tiny-imagenet-200/train', transform=train_transform)
+    
+    # if train_sample_size is not None:
+    #     # Randomly sample a subset of the training set
+    #     indices = torch.randperm(len(trainset))[:train_sample_size]
+    #     trainset = torch.utils.data.Subset(trainset, indices)
+
+    # trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+    #                                         shuffle=True, num_workers=num_workers)
+    
+    # test_transform = transforms.Compose(
+    #     [transforms.ToTensor(),
+    #     # transforms.Resize((224, 224)),
+    #     transforms.Resize((64, 64)),
+    #     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+
+    # testset = torchvision.datasets.ImageFolder('tiny-imagenet-200/val', transform=test_transform)
+    
+    # if test_sample_size is not None:
+    #     # Randomly sample a subset of the test set
+    #     indices = torch.randperm(len(testset))[:test_sample_size]
+    #     testset = torch.utils.data.Subset(testset, indices)
+
+    # testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+    #                                         shuffle=False, num_workers=num_workers)
+
+
 
     classes = list(range(0, 200))
     
