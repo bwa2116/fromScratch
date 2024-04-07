263fae3ae232ce2d969a928207ecd043e8eecb88
bwa2116 <151790222+bwa2116@users.noreply.github.com>
2024-04-07 10:41:20 +0200

    Update config.py

--- config.py
+++ config.py
@@ -56,14 +56,14 @@ config_Places365 = {
 
 config_ImageNet200 = {
     "patch_size": patch_size,
-    "hidden_size": (32**2)/patch_size,
+    "hidden_size": (64**2)/patch_size,
     "num_hidden_layers": num_hidden_layers,
     "num_attention_heads": num_attention_heads,
     "intermediate_size": 4 * hidden_size,
     "hidden_dropout_prob": hidden_dropout_prob,
     "attention_probs_dropout_prob": attention_probs_dropout_prob,
     "initializer_range": initializer_range,
-    "image_size": 32, # image_size of ImageNet - variable - convention is 224 but CUDA memory
+    "image_size": 64, # image_size of ImageNet - variable - convention is 224 but CUDA memory
     "num_classes": 200, # num_classes of ImageNet
     "num_channels": 3, # num_channels of ImageNet - color
     "qkv_bias": qkv_bias,
